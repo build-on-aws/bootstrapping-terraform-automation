78ba94655d710efd6dca45cc527259b4618c4c4c
Cobus Bernard <cobusb@amazon.com>
2023-03-09 00:14:32 +0000

    Changing bootstrapping backend config to not be commented out, and running fmt

--- _bootstrap/codecatalyst/terraform.tf
+++ _bootstrap/codecatalyst/terraform.tf
@@ -1,21 +1,21 @@
 # Commented out on purpose as you need to set up the required AWS resources before
 # you can store the state file.
 
-# terraform {
-#   backend "s3" {
-#     bucket         = "tf-state-files"
-#     key            = "terraform-bootstrap-state-file/terraform.tfstate"
-#     region         = "us-east-1"
-#     dynamodb_table = "TerraformMainStateLock"
-#     kms_key_id     = "alias/s3" # Optionally change this to the custom KMS alias you created - "alias/terraform"
-#   }
+terraform {
+  backend "s3" {
+    bucket         = "tf-state-files"
+    key            = "terraform-bootstrap-state-file/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "TerraformMainStateLock"
+    kms_key_id     = "alias/s3" # Optionally change this to the custom KMS alias you created - "alias/terraform"
+  }
 
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 4.33"
-#     }
-#   }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.33"
+    }
+  }
 
-#   required_version = "= 1.3.7"
-# }
+  required_version = "= 1.3.7"
+}
